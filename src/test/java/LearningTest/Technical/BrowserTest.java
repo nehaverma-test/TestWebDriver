61392825d2fda233cb344ee2780312cbace6c6cf
“Neha <Neha.verma@kineo.com.au>
2020-06-23 21:17:08 +0530

    Initial commit

---
+++ src/test/java/LearningTest/Technical/BrowserTest.java
@@ -0,0 +1,28 @@
+package LearningTest.Technical;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class BrowserTest {
+	
+	
+	@Test
+	public void launch()
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/admin/Desktop/Test/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.get("https://jqueryui.com/droppable/");
+		
+		System.out.println("Title is = "+driver.getTitle());
+	}
+
+}
---
+++ src/test/java/LearningTest/Technical/DisplayTest.java
@@ -0,0 +1,15 @@
+package LearningTest.Technical;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class DisplayTest {
+
+	
+	@Test
+	public void displayFunction()
+	{
+		System.out.println("We are inside Display function");
+	}
+}
